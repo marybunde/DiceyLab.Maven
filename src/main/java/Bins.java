1e9c7eeebd8c4ef70c9d6041e70130117e739972
marybunde <marybunde@gmail.com>
2022-07-29 22:38:38 -0400

    bins class done

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,15 @@
 
 public class Bins {
+    int [] bin;
 
+    public Bins(int high) {
+        int [] arrBin = new int[high + 1];
+        this.bin = arrBin;
+    }
+    public void incrementBin(int index) {
+        bin[index] ++;
+    }
+    public Integer getBin(Integer index){
+        return bin[index];
+    }
 }
