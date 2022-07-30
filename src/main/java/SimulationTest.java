5ac1962e06c473763cc69a9138da7461ca490201
marybunde <marybunde@gmail.com>
2022-07-30 00:06:07 -0400

    simulation test done

--- src/main/java/SimulationTest.java
+++ src/main/java/SimulationTest.java
@@ -1,2 +1,15 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class SimulationTest {
+    @Test
+    public void SimulationTest() {
+        int numberOfDice = 1;
+        Simulation simulation = new Simulation(numberOfDice, 2);
+        simulation.runSimulation();
+        int actual = simulation.result.bin[numberOfDice-1];
+        int expected = 0;
+        Assert.assertEquals(expected,actual);
+
+    }
 }
