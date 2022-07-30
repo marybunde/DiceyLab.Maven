fb39b9b0714b6f8dd44d1a573d344239af60e120
marybunde <marybunde@gmail.com>
2022-07-29 23:55:07 -0400

    simulation done

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,19 @@
 public class Simulation {
+    Dice RollDie;
+    Integer numOfRolls;
+    Bins result;
 
+    public Simulation(Integer numberOfDie, Integer numberOfRolls) {
+        RollDie = new Dice(numberOfDie);
+        numOfRolls = numberOfRolls;
+        result = new Bins(numberOfDie * 6);
+    }
+    public void runSimulation(){
+        for (int i = 0; i < numOfRolls; i++) {
+            int singleRoll = RollDie.rollDie();
+            result.incrementBin(singleRoll);
+        }
+    }
 
 
 }
